823fc4a74e947c7f0cb27a2dd09a1dacaa79ad48
Julian Principe <161102679+AndroidAndFlutterDev@users.noreply.github.com>
2024-12-14 13:50:09 -0300

    Update Map.py

--- Map.py
+++ Map.py
@@ -2,10 +2,15 @@
 
 import folium
 
+# Buenos Aires location (you can choose whatever you want, like New York)
 location = (-34.6037,-58.3816)
 
 map = folium.Map(location = location, zoom_start=12)
 
+# Add the marker to the map
 folium.Marker(location, popup='CABA').add_to(map)
 
+# Save it as map.html
 map.save('map.html')
+
+print("The map.html has been generated. Open it in your browser")
